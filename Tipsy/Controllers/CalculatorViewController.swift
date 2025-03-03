eb1fcf90ccdda0530900292a89d551c493c4856e
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-03-03 03:15:54 -0500

    tip buttons

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
+    var tip = 0.1
+    
     //MARK: Outlets
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
@@ -19,9 +21,35 @@ class CalculatorViewController: UIViewController {
     
     //MARK: Actions
     @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+
+        sender.isSelected = true
+        
+        let buttonTitle = sender.currentTitle!
+        let buttonTitleRemovePercentageSign = String(buttonTitle.dropLast())
+        let buttonTitleAsNumber = Double(buttonTitleRemovePercentageSign)!
+        
+        tip = buttonTitleAsNumber / 100
+        if zeroPctButton.isSelected == true {
+//            tenPctButton.isSelected = false
+//            twentyPctButton.isSelected = false
+            print("No tip")
+        } else if tenPctButton.isSelected == true {
+//            zeroPctButton.isSelected = false
+//            twentyPctButton.isSelected = false
+            print("10% was selected")
+        } else {
+//            zeroPctButton.isSelected = false
+//            tenPctButton.isSelected = false
+            print("20% was selected")
+        }
     }
+    
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
+    
     @IBAction func calculatePressed(_ sender: UIButton) {
     }
     
